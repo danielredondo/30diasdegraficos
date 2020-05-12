c75ede0d24a90879180da7de4815d33213dc1a0b
Daniel Redondo <danielredondosanchez@hotmail.com>
2020-05-12 15:24:35 +0200

    añadir_carpetas

---
+++ scripts/1_barras.R
@@ -0,0 +1,35 @@
+# ----- Día 1: barras ----- 
+library(ggplot2)
+library(dplyr)
+
+#Importar ENSE -> https://danielredondo.com/posts/20181010_ens/
+load("datasets/ense2017.RData")
+
+# Quitar NAs
+tabla <- tabla %>% filter(S110<998) %>% filter(!is.na(S110))
+tabla <- tabla %>% filter(S109<998) %>% filter(!is.na(S109))
+
+tabla <- tabla %>% mutate(imc = 10000 * S110/(S109^2))
+
+summary(tabla$imc)
+
+ggplot(tabla, aes(x = imc)) +
+  geom_histogram(aes(y = (..count..)/sum(..count..)), col = "black", fill = "firebrick", binwidth = 1) + 
+  ggtitle("Índice de masa corporal en adultos (Encuesta Nacional de Salud, 2017)") + 
+  scale_x_continuous(name = "Índice de masa corporal", breaks = seq(10, 70, 5),
+                     labels = seq(10, 70, 5), limits = c(10, 70)) +
+  scale_y_continuous(name = "Porcentaje de la población", labels = scales::percent) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(color = "black"),
+    axis.text.y = element_text(color = "black"),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    plot.background = element_rect(fill = "white", color = NA),
+    panel.background = element_rect(fill = "white", color = NA),
+    legend.background = element_rect(fill = "white", color = NA),
+    #panel.border = element_blank(),
+    plot.title = element_text(size = 15, hjust = 0.5, face = "bold")
+  )
+
+ggsave("1.png")
