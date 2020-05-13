235cc4899278da071aa987480a03a9daf963013f
Daniel Redondo <danielredondosanchez@hotmail.com>
2020-05-14 00:25:03 +0200

    dia3

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # #30díasdegráficos
 
+#30díasdegráficos es una iniciativa de [R4ds en español](https://github.com/cienciadedatos/datos-de-miercoles/blob/master/30-dias-de-graficos-2020.md) para conmemorar el nacimiento de Florence Nightingale, en el que se propone crear un gráfico distinto al día.
+
 ### Día 1. Gráficos de barras
 
 Histograma del índice de masa corporal en adultos, según la Encuesta Nacional de Salud de 2017. [Código](scripts/1_barras.R).
@@ -11,3 +13,9 @@ Histograma del índice de masa corporal en adultos, según la Encuesta Nacional 
 Número de visitas diarias a la página de R en Wikipedia en español desde 2018. [Código](scripts/2_lineas.R).
 
 ![](graficos/2.png)
+
+### Día 3. Gráficos de puntos/burbujas
+
+Distribución de los premios de lotería de 2018. [Código](scripts/3_burbujas.R).
+
+![](graficos/3.png)
---
+++ scripts/3_burbujas.R
@@ -0,0 +1,36 @@
+# ----- Día 3: Burbujas -----
+
+# https://danielredondo.com/posts/20180215_vis_navidad/
+
+library(readr)
+library(ggplot2)
+library(scales)
+
+# Lectura fichero
+loteria <- read_delim("datasets/loteria.csv",
+  delim = ";",
+  escape_double = FALSE,
+  col_names = FALSE,
+  col_types = cols(
+    `X1` = col_integer(),
+    `X2` = col_integer(),
+    `X3` = col_skip()
+  ), trim_ws = TRUE
+)
+names(loteria) <- c("numero", "premio")
+
+# Gráfico
+point <- format_format(big.mark = ".", decimal.mark = ",", scientific = FALSE)
+ggplot(loteria, aes(loteria$numero, loteria$premio)) +
+  geom_point(size = sqrt(loteria$premio / 5000),
+             col = "firebrick3") +
+  geom_text(data = subset(loteria, loteria$premio > 59000),
+            aes(numero, premio, label = point(numero)),
+            col = "white",
+            size = sqrt(subset(loteria, loteria$premio > 59000)$premio) / 300) +
+  scale_y_continuous(name = "Premio (€)", labels = point, limits = c(0, 4800000)) +
+  scale_x_continuous(name = "Número", labels = point, limits = c(0, 100000),
+                     breaks = seq(0, 100000, 10000), minor_breaks = seq(0, 100000, 10000)) +
+  theme(panel.background = element_rect(fill = "gray97"))
+
+ggsave("3.png")
