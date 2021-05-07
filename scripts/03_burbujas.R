a78ca56f9fe1feb95791a82e0371f462161ae301
Daniel Redondo <danielredondosanchez@hotmail.com>
2021-05-07 14:03:57 +0200

    mejorar gráfico día 3

--- scripts/03_burbujas.R
+++ scripts/03_burbujas.R
@@ -1,10 +1,13 @@
 # ----- Día 3: Burbujas -----
-
 # https://danielredondo.com/posts/20180215_vis_navidad/
-
 library(readr)
 library(ggplot2)
 library(scales)
+
+# Para fuentes
+library(extrafont)
+# font_import()
+loadfonts(device = "win")
 
 # Lectura fichero
 loteria <- read_delim("datasets/loteria.csv",
@@ -21,18 +24,33 @@ names(loteria) <- c("numero", "premio")
 
 # Gráfico
 point <- format_format(big.mark = ".", decimal.mark = ",", scientific = FALSE)
-ggplot(loteria, aes(loteria$numero, loteria$premio)) +
-  geom_point(size = sqrt(loteria$premio / 5000),
-             col = "firebrick3") +
-  geom_text(data = subset(loteria, loteria$premio > 59000),
-            aes(numero, premio, label = point(numero)),
-            col = "white",
-            size = sqrt(subset(loteria, loteria$premio > 59000)$premio) / 300) +
-  scale_y_continuous(name = "Premio (€)", labels = point, limits = c(0, 4800000),
-                     expand = c(0, 0)) +
-  scale_x_continuous(name = "Número", labels = point, limits = c(0, 100000),
-                     breaks = seq(0, 100000, 10000), minor_breaks = seq(0, 100000, 10000),
-                     expand = c(0, 0)) +
-  theme_minimal()
 
-ggsave("3.png")
+ggplot(loteria, aes(loteria$numero, loteria$premio)) +
+  geom_point(
+    size = sqrt(loteria$premio / 5000),
+    col = "firebrick3"
+  ) +
+  geom_text(
+    data = subset(loteria, loteria$premio > 59000),
+    aes(numero, premio, label = point(numero)),
+    col = "white", family = "Forte",
+    size = sqrt(subset(loteria, loteria$premio > 59000)$premio) / 250
+  ) +
+  scale_y_continuous(
+    name = "Premio (€)", labels = point, limits = c(0, 4800000),
+    expand = c(0, 0)
+  ) +
+  scale_x_continuous(
+    name = "Número", labels = point, limits = c(0, 102000),
+    breaks = seq(0, 100000, 10000), minor_breaks = seq(0, 100000, 10000),
+    expand = c(0, 0)
+  ) +
+  theme_minimal() +
+  theme(
+    text = element_text(family = "Forte"),
+    axis.text = element_text(color = "black"),
+    axis.title = element_text(size = 15)
+  )
+
+# Exportar gráfico
+ggsave("3.png", width = 8, height = 5)
