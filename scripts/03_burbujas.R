4d7e34d2ba4881d032a4d6a8ec99cddc3c52ed62
Daniel Redondo <danielredondosanchez@hotmail.com>
2021-05-07 13:59:16 +0200

    mejorar gráfico 3

--- scripts/03_burbujas.R
+++ scripts/03_burbujas.R
@@ -8,14 +8,14 @@ library(scales)
 
 # Lectura fichero
 loteria <- read_delim("datasets/loteria.csv",
-  delim = ";",
-  escape_double = FALSE,
-  col_names = FALSE,
-  col_types = cols(
-    `X1` = col_integer(),
-    `X2` = col_integer(),
-    `X3` = col_skip()
-  ), trim_ws = TRUE
+                      delim = ";",
+                      escape_double = FALSE,
+                      col_names = FALSE,
+                      col_types = cols(
+                        `X1` = col_integer(),
+                        `X2` = col_integer(),
+                        `X3` = col_skip()
+                      ), trim_ws = TRUE
 )
 names(loteria) <- c("numero", "premio")
 
@@ -28,9 +28,11 @@ ggplot(loteria, aes(loteria$numero, loteria$premio)) +
             aes(numero, premio, label = point(numero)),
             col = "white",
             size = sqrt(subset(loteria, loteria$premio > 59000)$premio) / 300) +
-  scale_y_continuous(name = "Premio (€)", labels = point, limits = c(0, 4800000)) +
+  scale_y_continuous(name = "Premio (€)", labels = point, limits = c(0, 4800000),
+                     expand = c(0, 0)) +
   scale_x_continuous(name = "Número", labels = point, limits = c(0, 100000),
-                     breaks = seq(0, 100000, 10000), minor_breaks = seq(0, 100000, 10000)) +
-  theme(panel.background = element_rect(fill = "gray97"))
+                     breaks = seq(0, 100000, 10000), minor_breaks = seq(0, 100000, 10000),
+                     expand = c(0, 0)) +
+  theme_minimal()
 
 ggsave("3.png")
